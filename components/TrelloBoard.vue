32d3082325f740a6678b12e20ba544bd11e247b1
zhang.weifeng <zhangweifeng@diinom.com>
2025-07-22 10:30:06 +0800

    feat: 同步课程代码

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -1,7 +1,58 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import draggable from "vuedraggable";
+import {nanoid} from "nanoid";
+import type {Column} from "~~/types";
 
+const columns = ref<Column[]>([
+  {
+    title: "Backlog",
+    id: nanoid(),
+    tasks: [
+      {
+        title: "Create marketing landing page",
+        createdAt: new Date(),
+        id: nanoid(),
+      },
+      {
+        title: "Develop cool new feature",
+        createdAt: new Date(),
+        id: nanoid(),
+      },
+      {title: "Fix page nav bug", createdAt: new Date(), id: nanoid()},
+    ],
+  },
+  {title: "Selected for Dev", id: nanoid(), tasks: []},
+  {title: "In Progress", id: nanoid(), tasks: []},
+  {title: "QA", id: nanoid(), tasks: []},
+  {title: "Complete", id: nanoid(), tasks: []},
+]);
+</script>
 <template>
-  <div />
+  <div>
+    <draggable
+      v-model="columns"
+      group="columns"
+      :animation="150"
+      handle=".drag-handle"
+      item-key="id"
+      class="flex gap-4 overflow-x-auto items-start"
+    >
+      <template #item="{element: column}: {element: Column}">
+        <div class="column bg-gray-200 p-5 rounded min-w-[250px]">
+          <header class="font-bold mb-4">
+            <DragHandle />
+            {{ column.title }}
+          </header>
+          <TrelloBoardTask
+            v-for="task in column.tasks"
+            :key="task.id"
+            :task="task"
+          />
+          <footer>
+            <button class="text-gray-500">+ Add a Card</button>
+          </footer>
+        </div>
+      </template>
+    </draggable>
+  </div>
 </template>
-
-<style scoped></style>
